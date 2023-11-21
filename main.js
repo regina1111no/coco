39f0566bf82894cc6d9750ad36a064f24bc37b2e
regina1111no <125106376+regina1111no@users.noreply.github.com>
2023-11-21 17:50:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 img = "";
+objects=[];
+status="";
 
 function preload(){
   img = loadImage('dog_cat.jpg');
 }
 function setup(){
-    canvas=createCanvas(700,600);
+    canvas=createCanvas(700,550);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("etiqueta").innerHTML = "Estado : Detección de objetos";
+}
+function modelLoaded(){
+    console.log("tu modelo ya cargo 💛");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
 }
 function draw(){
         image(img, 0, 0, 700, 600);
@@ -14,4 +30,9 @@ function draw(){
         noFill();
         stroke("#FF0000");
         rect(30, 60, 450, 350 );
+        fill("#FF0000");
+        text("gato🐈‍⬛", 345, 185);
+        noFill();
+        stroke("#FF0000");
+        rect(320, 100, 360, 450 );
 }
