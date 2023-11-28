323cfc5d73ddd843428938b65f68acb918cac8e7
regina1111no <125106376+regina1111no@users.noreply.github.com>
2023-11-28 17:57:14 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ function preload(){
   img = loadImage('https://th.bing.com/th/id/OIP.QxO6h7JSyLLns6iFPIMCqgHaHa?rs=1&pid=ImgDetMain');
 }
 function setup(){
-    canvas=createCanvas(700,550);
+    canvas=createCanvas(500,400);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(500,400);
+    video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("etiqueta").innerHTML = "Estado : Detección de objetos";
 }
 function modelLoaded(){
     console.log("tu modelo ya cargo 💛");
     status=true;
-    objectDetector.detect(img,gotResult);
 }
 function gotResult(error, results){
     if(error){
@@ -24,19 +26,23 @@ function gotResult(error, results){
     objects=results;
 }
 function draw(){
-        image(img, 0, 0, 700, 600);
+        image(video, 0, 0, 500, 400);
         if(status != "")
       {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
         for(i=0; i<objects.length; i++)
         {
-            document.getElementById("etiqueta").innerHTML="objetos detectados";
-        
 
-            fill("#FF0000");
+            document.getElementById("numeroobjetos").innerHTML="numero de objetos detectados:"+objects.length;        
+
+            fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
-            stroke("#FF0000");
+            stroke(r,g,b);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);        
         }
      }
